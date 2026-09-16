a38c84f694a157ce9be9aa2392ee242d21a5b2a9
rafaellidepaula1 <rafaellidepaula1@gmail.com>
2026-09-16 14:52:21 -0300

    16/08

---
+++ atividade4_dom/script.js
@@ -0,0 +1,11 @@
+const titulo = document.getElementById('spantexto');
+const botaoAlterartexto = document.getElementById('btnAlterartexto');
+const numero1 = document.getElementById('inputNumero1');
+const botaoCalcular = document.getElementById('Btncalcalculos');
+const texto1 = document.getElementById('resultado');
+
+document.adicionar
+  
+
+
+
